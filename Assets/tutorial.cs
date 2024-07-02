0432cebe0e23b6b7938b78e8426ad851d9b5a6c8
Claire Thananopavarn <cthanano@usc.edu>
2024-07-01 22:46:55 -0400

    tutorial

--- Assets/tutorial.cs
+++ Assets/tutorial.cs
@@ -4,15 +4,88 @@ using UnityEngine;
 
 public class tutorial : MonoBehaviour
 {
+    bool clicked = false;
+    public Sprite question;
+    public Sprite x;
+    public GameObject forward;
+    public GameObject one;
+    public GameObject two;
+    public GameObject three;
+    public GameObject four;
+    public GameObject five;
+    public GameObject six;
+    List<GameObject> pages =  new List<GameObject>();
+    public dragAll drag;
+
+    GameObject[] titles;
+    int page = 1;
+    public GameObject back;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach (GameObject tut in GameObject.FindGameObjectsWithTag("tutorial"))
+            tut.SetActive(false);
+        titles = GameObject.FindGameObjectsWithTag("title");
+        page = 1;
+        pages.Add(one);
+        pages.Add(two);
+        pages.Add(three);
+        pages.Add(four);
+        pages.Add(five);
+        pages.Add(six);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Click() {
+        if (clicked) {
+            clicked = false;
+            foreach (GameObject tut in GameObject.FindGameObjectsWithTag("tutorial")) {
+                tut.SetActive(false);
+            }
+            GetComponent<UnityEngine.UI.Image>().sprite = question;
+            foreach (GameObject title in titles) {
+                title.SetActive(true);
+            }
+            if (drag)
+                drag.setEnable(true);
+        } else {
+            clicked = true;
+            forward.SetActive(true);
+            one.SetActive(true);
+            GetComponent<UnityEngine.UI.Image>().sprite = x;
+            foreach (GameObject ob in titles) {
+                ob.SetActive(false);
+            }
+            page = 1;
+            if (drag)
+                drag.setEnable(false);
+        }
+    }
+
+    public void next() {
+        page++;
+        if (page == 6)
+            forward.SetActive(false);
+        if (page == 2)
+            back.SetActive(true);
+        pages[page-1].SetActive(true);
+        pages[page-2].SetActive(false);
+    }
+
+    public void prev() {
+        page--;
+        if (page == 5)
+            forward.SetActive(true);
+        if (page == 1)
+            back.SetActive(false);
+        pages[page-1].SetActive(true);
+        pages[page].SetActive(false);
     }
 }
